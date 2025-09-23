3cdacfa56196e51bc2d4088175e05af9977045dd
KaliaGonzalez <k.gonzalezj@uniandes.edu.co>
2025-09-23 06:57:26 -0500

    hola

--- src/main/page.tsx
+++ src/main/page.tsx
@@ -11,7 +11,7 @@ export default async function HomePage() {
         <div> 
             <div className="flex flex-col items-center justify-center min-h-screen py-2"> 
                 <h1 className="text-4xl font-bold mb-8">Rick and Morty Episodes</h1> 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"> 
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-8"> 
                     {episodes.results.map((episode: { id: number; name: string; air_date: string; episode: string }) => ( 
                         <div key={episode.id} className="bg-white rounded-lg shadow-md p-6"> 
                             <h2 className="text-2xl font-semibold mb-2">{episode.name}</h2> 
